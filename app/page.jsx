29e5cfaaa5e3ef266ca099c2fd017695a0d4ea81
priyankarathor <priyankaji856@gmail.com>
2026-01-19 18:19:30 +0530

    page.jsx

--- app/page.jsx
+++ app/page.jsx
@@ -1,14 +1,5 @@
-// app/page.jsx  (Server Component by default)
-import Button from "@/components/InteractiveButton";
-
 export default function Page() {
-  const handleClick = () => {
-    console.log("clicked");
-  };
-
   return (
-    <Button onClick={handleClick}>
-      Click me
-    </Button>
+    <h1>Hello</h1>
   );
 }
